4298a421a7328ab7e5a17085e32e76680c6937db
Edward Tonai <edward.tonai@gmail.com>
2025-08-10 01:14:05 -0700

    Updated build in the middle of dev cycle for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.4"
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
